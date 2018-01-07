452a22166580dc23873aaf4cdcfd1b033e0efaf0
Cláudio "Feralidragon" Luís <tutano2004@gmail.com>
2018-01-07 13:13:08 +0000

    Prototype exceptions DRY enhancement

--- src/Feralygon/Kit/Core/Prototype/Exception.php
+++ src/Feralygon/Kit/Core/Prototype/Exception.php
@@ -8,11 +8,35 @@
 namespace Feralygon\Kit\Core\Prototype;
 
 use Feralygon\Kit\Core;
+use Feralygon\Kit\Core\Prototype;
+use Feralygon\Kit\Core\Utilities\Type as UType;
 
 /**
  * Core prototype exception class.
  * 
  * @since 1.0.0
+ * @property-read \Feralygon\Kit\Core\Prototype $prototype <p>The prototype instance.</p>
  * @see \Feralygon\Kit\Core\Prototype
  */
-abstract class Exception extends Core\Exception {}
+abstract class Exception extends Core\Exception
+{
+	//Implemented public static methods
+	/** {@inheritdoc} */
+	public static function getRequiredPropertyNames() : array
+	{
+		return ['prototype'];
+	}
+	
+	
+	
+	//Implemented protected methods
+	/** {@inheritdoc} */
+	protected function evaluateProperty(string $name, &$value) : ?bool
+	{
+		switch ($name) {
+			case 'prototype':
+				return is_object($value) && UType::isA($value, Prototype::class);
+		}
+		return null;
+	}
+}
--- src/Feralygon/Kit/Core/Prototype/Exceptions/PropertiesNotImplemented.php
+++ src/Feralygon/Kit/Core/Prototype/Exceptions/PropertiesNotImplemented.php
@@ -7,9 +7,7 @@
 
 namespace Feralygon\Kit\Core\Prototype\Exceptions;
 
-use Feralygon\Kit\Core\Prototype;
 use Feralygon\Kit\Core\Prototype\Exception;
-use Feralygon\Kit\Core\Utilities\Type as UType;
 
 /**
  * Core prototype properties not implemented exception class.
@@ -17,7 +15,6 @@ use Feralygon\Kit\Core\Utilities\Type as UType;
  * This exception is thrown from a prototype whenever properties are given but there is nothing implemented to handle them.
  * 
  * @since 1.0.0
- * @property-read \Feralygon\Kit\Core\Prototype $prototype <p>The prototype instance.</p>
  */
 class PropertiesNotImplemented extends Exception
 {
@@ -27,27 +24,5 @@ class PropertiesNotImplemented extends Exception
 	{
 		return "Properties not implemented in prototype {{prototype}}.\n" . 
 			"HINT: In order to use properties, the properties interface must be implemented by this prototype.";
-	}
-	
-	
-	
-	//Implemented public static methods
-	/** {@inheritdoc} */
-	public static function getRequiredPropertyNames() : array
-	{
-		return ['prototype'];
-	}
-	
-	
-	
-	//Implemented protected methods
-	/** {@inheritdoc} */
-	protected function evaluateProperty(string $name, &$value) : ?bool
-	{
-		switch ($name) {
-			case 'prototype':
-				return is_object($value) && UType::isA($value, Prototype::class);
-		}
-		return null;
 	}
 }
