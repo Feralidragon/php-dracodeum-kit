2217f00f95dc54d4321485cdc11b50df9426d1f9
Cláudio "Feralidragon" Luís <tutano2004@gmail.com>
2018-01-16 16:29:30 +0000

    Input lowercase and uppercase constraints

---
+++ src/Feralygon/Kit/Core/Prototypes/Input/Prototypes/Modifiers/Constraints/Uppercase.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * @author Cláudio "Feralidragon" Luís <claudio.luis@aptoide.com>
+ * @license https://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Feralygon\Kit\Core\Prototypes\Input\Prototypes\Modifiers\Constraints;
+
+use Feralygon\Kit\Core\Prototypes\Input\Prototypes\Modifiers\Constraint;
+use Feralygon\Kit\Core\Prototype\Interfaces\Properties as IPrototypeProperties;
+use Feralygon\Kit\Core\Prototypes\Input\Prototypes\Modifier\Interfaces\{
+	Name as IName,
+	Information as IInformation,
+	SchemaData as ISchemaData
+};
+use Feralygon\Kit\Core\Traits\ExtendedProperties\Objects\Property;
+use Feralygon\Kit\Core\Options\Text as TextOptions;
+use Feralygon\Kit\Core\Utilities\{
+	Text as UText,
+	Type as UType
+};
+
+/**
+ * Core input uppercase constraint modifier prototype class.
+ * 
+ * This input constraint modifier prototype restricts a value to uppercase.
+ * 
+ * @since 1.0.0
+ * @property bool $unicode [default = false] <p>Check as an Unicode value.</p>
+ */
+class Uppercase extends Constraint implements IPrototypeProperties, IName, IInformation, ISchemaData
+{
+	//Private properties
+	/** @var bool */
+	private $unicode = false;
+	
+	
+	
+	//Implemented public methods
+	/** {@inheritdoc} */
+	public function checkValue($value) : bool
+	{
+		return $value === UText::upper($value, $this->unicode);
+	}
+	
+	
+	
+	//Implemented public methods (core prototype properties interface)
+	/** {@inheritdoc} */
+	public function buildProperty(string $name) : ?Property
+	{
+		switch ($name) {
+			case 'unicode':
+				return $this->createProperty()
+					->setEvaluator(function (&$value) : bool {
+						return UType::evaluateBoolean($value);
+					})
+					->setGetter(function () : bool {
+						return $this->unicode;
+					})
+					->setSetter(function (bool $unicode) : void {
+						$this->unicode = $unicode;
+					})
+				;
+		}
+		return null;
+	}
+	
+	
+	
+	//Implemented public static methods (core prototype properties interface)
+	/** {@inheritdoc} */
+	public static function getRequiredPropertyNames() : array
+	{
+		return [];
+	}
+	
+	
+	
+	//Implemented public methods (core input modifier prototype name interface)
+	/** {@inheritdoc} */
+	public function getName() : string
+	{
+		return 'constraints.uppercase';
+	}
+	
+	
+	
+	//Implemented public methods (core input modifier prototype information interface)
+	/** {@inheritdoc} */
+	public function getLabel(TextOptions $text_options) : string
+	{
+		/**
+		 * @description Core input uppercase constraint modifier prototype label.
+		 * @tags core prototype input modifier constraint uppercase label
+		 */
+		return UText::localize("Uppercase only", 'core.prototypes.input.prototypes.modifiers.constraints.uppercase', $text_options);
+	}
+	
+	/** {@inheritdoc} */
+	public function getMessage(TextOptions $text_options) : string
+	{
+		/**
+		 * @description Core input uppercase constraint modifier prototype message.
+		 * @tags core prototype input modifier constraint uppercase message
+		 */
+		return UText::localize("Only values in uppercase are allowed.", 'core.prototypes.input.prototypes.modifiers.constraints.uppercase', $text_options);
+	}
+	
+	
+	
+	//Implemented public methods (core input modifier prototype schema data interface)
+	/** {@inheritdoc} */
+	public function getSchemaData()
+	{
+		return [
+			'unicode' => $this->unicode
+		];
+	}
+}
