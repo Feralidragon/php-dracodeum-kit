36619b12a745fa0c33bc652084b9fe6a68eb7870
Cláudio "Feralidragon" Luís <tutano2004@gmail.com>
2018-01-29 18:20:09 +0000

    Options new readonly instantiation parameter

--- src/Feralygon/Kit/Core/Options.php
+++ src/Feralygon/Kit/Core/Options.php
@@ -19,7 +19,8 @@ use Feralygon\Kit\Core\Utilities\Type as UType;
  * and is meant to be mainly used within a class method or function, 
  * by representing an additional set of optional parameters.<br>
  * All properties are validated and sanitized, guaranteeing their type and integrity, 
- * and may be retrieved and modified directly just like public object properties.
+ * and may be retrieved and modified directly just like public object properties, 
+ * and may also be set to read-only during instantiation to prevent any changes.
  * 
  * @since 1.0.0
  */
@@ -36,10 +37,13 @@ abstract class Options implements \ArrayAccess
 	 * 
 	 * @since 1.0.0
 	 * @param array $properties [default = []] <p>The properties, as <samp>name => value</samp> pairs.</p>
+	 * @param bool $readonly [default = false] <p>Set all properties as read-only.</p>
 	 */
-	final public function __construct(array $properties = [])
+	final public function __construct(array $properties = [], bool $readonly = false)
 	{
-		$this->initializeProperties($properties, \Closure::fromCallable([$this, 'evaluateProperty']));
+		$this->initializeProperties(
+			$properties, \Closure::fromCallable([$this, 'evaluateProperty']), [], $readonly ? 'r' : 'rw'
+		);
 	}
 	
 	
@@ -69,13 +73,16 @@ abstract class Options implements \ArrayAccess
 	 * @param mixed $value [reference] <p>The value to evaluate (validate and sanitize).</p>
 	 * @param bool $clone [default = false] <p>If an instance is given, 
 	 * clone it into a new one with the same properties.</p>
+	 * @param bool $readonly [default = false] <p>Set all properties of a new instance as read-only.</p>
 	 * @param bool $nullable [default = false] <p>Allow the given value to evaluate as <code>null</code>.</p>
 	 * @return bool <p>Boolean <code>true</code> if the given value is successfully evaluated into an instance.</p>
 	 */
-	final public static function evaluate(&$value, bool $clone = false, bool $nullable = false) : bool
+	final public static function evaluate(
+		&$value, bool $clone = false, bool $readonly = false, bool $nullable = false
+	) : bool
 	{
 		try {
-			$value = static::coerce($value, $clone, $nullable);
+			$value = static::coerce($value, $clone, $readonly, $nullable);
 		} catch (Exceptions\CoercionFailed $exception) {
 			return false;
 		}
@@ -92,23 +99,26 @@ abstract class Options implements \ArrayAccess
 	 * @param mixed $value <p>The value to coerce (validate and sanitize).</p>
 	 * @param bool $clone [default = false] <p>If an instance is given, 
 	 * clone it into a new one with the same properties.</p>
+	 * @param bool $readonly [default = false] <p>Set all properties of a new instance as read-only.</p>
 	 * @param bool $nullable [default = false] <p>Allow the given value to coerce as <code>null</code>.</p>
 	 * @throws \Feralygon\Kit\Core\Options\Exceptions\CoercionFailed
 	 * @return static|null <p>The given value coerced into an instance.<br>
 	 * If nullable, <code>null</code> may also be returned.</p>
 	 */
-	final public static function coerce($value, bool $clone = false, bool $nullable = false) : ?Options
+	final public static function coerce(
+		$value, bool $clone = false, bool $readonly = false, bool $nullable = false
+	) : ?Options
 	{
 		try {
 			if (!isset($value)) {
-				return $nullable ? null : new static();
+				return $nullable ? null : new static([], $readonly);
 			} elseif (is_array($value)) {
-				return new static($value);
+				return new static($value, $readonly);
 			} elseif (is_object($value)) {
 				if (!$clone && get_class($value) === static::class) {
 					return $value;
 				} elseif (UType::isA($value, self::class)) {
-					return new static($value->getLoadedProperties());
+					return new static($value->getLoadedProperties(), $readonly);
 				}
 			}
 		} catch (\Exception $exception) {
