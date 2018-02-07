c95db2ca95375aae7951fae453ff9b35f784885b
Cláudio "Feralidragon" Luís <tutano2004@gmail.com>
2018-02-07 00:19:16 +0000

    Properties complete refactor (continuation)

--- src/Feralygon/Kit/Core/Managers/Properties.php
+++ src/Feralygon/Kit/Core/Managers/Properties.php
@@ -56,7 +56,8 @@ class Properties
 	 * therefore when retrieving a list of all properties, only a list of the currently loaded ones is returned.</p>
 	 * @param string|null $mode [default = null] <p>The read and write mode to set for all properties, 
 	 * which, if set, must be one the following:<br>
-	 * &nbsp; &#8226; &nbsp; <samp>rw</samp> : Allow all properties to be both read from and written to (read-write).<br>
+	 * &nbsp; &#8226; &nbsp; <samp>rw</samp> : Allow all properties to be both read from 
+	 * and written to (read-write).<br>
 	 * &nbsp; &#8226; &nbsp; <samp>r</samp> : Allow all properties to be only read from (read-only).<br>
 	 * &nbsp; &#8226; &nbsp; <samp>w</samp> : Allow all properties to be only written to (write-only).<br>
 	 * &nbsp; &#8226; &nbsp; <samp>w-</samp> : Allow all properties to be only written to, 
@@ -87,17 +88,6 @@ class Properties
 	
 	//Final public methods
 	/**
-	 * Check if is initialized.
-	 * 
-	 * @since 1.0.0
-	 * @return bool <p>Boolean <code>true</code> if is initialized.</p>
-	 */
-	final public function isInitialized() : bool
-	{
-		return $this->initialized;
-	}
-	
-	/**
 	 * Add a given set of required property names.
 	 * 
 	 * The properties, corresponding to the given required property names added here, 
@@ -115,5 +105,36 @@ class Properties
 			throw new Exceptions\AlreadyInitialized(['manager' => $this]);
 		}
 		$this->required_map += array_fill_keys($names, true);
+		return $this;
+	}
+	
+	/**
+	 * Check if is initialized.
+	 * 
+	 * @since 1.0.0
+	 * @return bool <p>Boolean <code>true</code> if is initialized.</p>
+	 */
+	final public function isInitialized() : bool
+	{
+		return $this->initialized;
+	}
+	
+	/**
+	 * Initialize with a given set of properties.
+	 * 
+	 * @since 1.0.0
+	 * @param array $properties <p>The properties to initialize with, as <samp>name => value</samp> pairs.</p>
+	 * @return $this <p>This instance, for chaining purposes.</p>
+	 */
+	final public function initialize(array $properties) : Properties
+	{
+		
+		//TODO
+		
+		$this->initialized = true;
+		
+		//TODO
+		
+		return $this;
 	}
 }
