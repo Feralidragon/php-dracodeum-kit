a89dda5e474cce25dac02f908f08529f248d3c95
Cláudio "Feralidragon" Luís <tutano2004@gmail.com>
2018-02-12 18:56:39 +0000

    Properties complete refactor (structure)

--- src/Feralygon/Kit/Core/Structure.php
+++ src/Feralygon/Kit/Core/Structure.php
@@ -7,7 +7,7 @@
 
 namespace Feralygon\Kit\Core;
 
-use Feralygon\Kit\Core\Traits\ExtendedLazyProperties\Objects\Property;
+use Feralygon\Kit\Core\Interfaces\Arrayable as IArrayable;
 
 /**
  * Core structure class.
@@ -15,16 +15,16 @@ use Feralygon\Kit\Core\Traits\ExtendedLazyProperties\Objects\Property;
  * This class is the base to be extended from when creating a structure.<br>
  * <br>
  * A structure is a simple object which represents and stores multiple properties of multiple types.<br>
- * Each and every single one of its properties is lazy-loaded and is validated and sanitized, 
- * guaranteeing its type and integrity, and may be retrieved and modified directly just like any public object property.
+ * Each and every single one of its properties is validated and sanitized, guaranteeing its type and integrity, 
+ * and may be retrieved and modified directly just like any public object property.
  * 
  * @since 1.0.0
  * @see https://en.wikipedia.org/wiki/Struct_(C_programming_language)
  */
-abstract class Structure implements \ArrayAccess
+abstract class Structure implements \ArrayAccess, IArrayable
 {
 	//Traits
-	use Traits\ExtendedLazyProperties\ArrayAccess;
+	use Traits\Properties\ArrayableAccess;
 	
 	
 	
@@ -37,34 +37,17 @@ abstract class Structure implements \ArrayAccess
 	 */
 	final public function __construct(array $properties = [])
 	{
-		$this->initializeProperties(
-			$properties, \Closure::fromCallable([$this, 'buildProperty']), $this->getRequiredPropertyNames()
-		);
+		$this->initializeProperties(\Closure::fromCallable([$this, 'buildProperties']), $properties);
 	}
-	
-	
-	
-	//Abstract public static methods
-	/**
-	 * Get required property names.
-	 * 
-	 * All the required properties returned here must be given during instantiation.
-	 * 
-	 * @since 1.0.0
-	 * @return string[] <p>The required property names.</p>
-	 */
-	abstract public static function getRequiredPropertyNames() : array;
 	
 	
 	
 	//Abstract protected methods
 	/**
-	 * Build property instance for a given name.
+	 * Build properties.
 	 * 
 	 * @since 1.0.0
-	 * @param string $name <p>The property name to build for.</p>
-	 * @return \Feralygon\Kit\Core\Traits\ExtendedLazyProperties\Objects\Property|null 
-	 * <p>The built property instance for the given name or <code>null</code> if none was built.</p>
+	 * @return void
 	 */
-	abstract protected function buildProperty(string $name) : ?Property;
+	abstract protected function buildProperties() : void;
 }
