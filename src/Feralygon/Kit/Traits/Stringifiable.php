22dc84f6651d19e420f2e747f19c13d4b885cdda
Cláudio "Feralidragon" Luís <tutano2004@gmail.com>
2018-02-28 00:00:28 +0000

    Stringifiable trait

---
+++ src/Feralygon/Kit/Traits/Stringifiable.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @author Cláudio "Feralidragon" Luís <claudio.luis@aptoide.com>
+ * @license https://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Feralygon\Kit\Traits;
+
+use Feralygon\Kit\Interfaces\Stringifiable as IStringifiable;
+use Feralygon\Kit\Utilities\Call as UCall;
+
+/**
+ * Stringifiable trait.
+ * 
+ * This trait adds the ability for a class to be converted into a string, 
+ * by using the <code>Feralygon\Kit\Interfaces\Stringifiable</code> interface.
+ * 
+ * @since 1.0.0
+ */
+trait Stringifiable
+{
+	//Final public magic methods
+	/**
+	 * Convert this object into a string.
+	 * 
+	 * This method requires this object to have 
+	 * the <code>Feralygon\Kit\Interfaces\Stringifiable</code> interface implemented.
+	 * 
+	 * @since 1.0.0
+	 * @return string <p>This object converted into a string.</p>
+	 */
+	final public function __toString() : string
+	{
+		UCall::guard($this instanceof IStringifiable, [
+			'hint_message' => "This method requires the object {{object}} to have " . 
+				"the \"Feralygon\\Kit\\Interfaces\\Stringifiable\" interface implemented.",
+			'parameters' => ['object' => $this]
+		]);
+		return $this->toString();
+	}
+}
