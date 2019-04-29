58db7c04989ae0a79677bc2fb43ae398b302a437
Cláudio "Feralidragon" Luís <tutano2004@gmail.com>
2019-04-29 19:50:24 +0100

    Dictionary input (continuation)

--- src/Feralygon/Kit/Prototypes/Inputs/Dictionary.php
+++ src/Feralygon/Kit/Prototypes/Inputs/Dictionary.php
@@ -36,28 +36,28 @@ use Feralygon\Kit\Utilities\Text as UText;
  * &nbsp; &#8226; &nbsp; a JSON array or object.
  * 
  * @since 1.0.0
- * @property-write \Feralygon\Kit\Components\Input|null $input [writeonce] [default = null]
- * <p>The input instance to evaluate values with.</p>
  * @property-write \Feralygon\Kit\Components\Input|null $key_input [writeonce] [default = null]
  * <p>The input instance to evaluate keys with.</p>
+ * @property-write \Feralygon\Kit\Components\Input|null $input [writeonce] [default = null]
+ * <p>The input instance to evaluate values with.</p>
  * @see https://en.wikipedia.org/wiki/Associative_array
  * @see \Feralygon\Kit\Primitives\Dictionary
  * @see \Feralygon\Kit\Interfaces\Arrayable
  */
-class Dictionary extends Input implements IInformation, /*IErrorMessage,*/ ISchemaData, IModifierBuilder, IErrorUnset
+class Dictionary extends Input implements IInformation, IErrorMessage, ISchemaData, IModifierBuilder, IErrorUnset
 {
 	//Protected properties
 	/** @var \Feralygon\Kit\Components\Input|null */
-	protected $input = null;
-	
-	/** @var \Feralygon\Kit\Components\Input|null */
 	protected $key_input = null;
 	
-	/** @var array */
-	protected $error_values = [];
+	/** @var \Feralygon\Kit\Components\Input|null */
+	protected $input = null;
 	
 	/** @var array */
 	protected $error_keys = [];
+	
+	/** @var array */
+	protected $error_values = [];
 	
 	
 	
@@ -102,24 +102,13 @@ class Dictionary extends Input implements IInformation, /*IErrorMessage,*/ ISche
 		}
 		
 		//inputs
-		if (isset($this->input) || isset($this->key_input)) {
+		if (isset($this->key_input) || isset($this->input)) {
 			//evaluate
 			$i = 0;
 			$dict = $dictionary->clone()->clear();
 			foreach ($dictionary as $k => $v) {
 				//initialize
 				$has_error = false;
-				
-				//input
-				if (isset($this->input)) {
-					if ($this->input->setValue($v, true)) {
-						$v = $this->input->getValue();
-						$this->input->unsetValue();
-					} else {
-						$this->error_values[$i] = $v;
-						$has_error = true;
-					}
-				}
 				
 				//key input
 				if (isset($this->key_input)) {
@@ -128,6 +117,17 @@ class Dictionary extends Input implements IInformation, /*IErrorMessage,*/ ISche
 						$this->key_input->unsetValue();
 					} else {
 						$this->error_keys[$i] = $k;
+						$has_error = true;
+					}
+				}
+				
+				//input
+				if (isset($this->input)) {
+					if ($this->input->setValue($v, true)) {
+						$v = $this->input->getValue();
+						$this->input->unsetValue();
+					} else {
+						$this->error_values[$i] = $v;
 						$has_error = true;
 					}
 				}
@@ -144,7 +144,7 @@ class Dictionary extends Input implements IInformation, /*IErrorMessage,*/ ISche
 			unset($dict);
 			
 			//check
-			if (!empty($this->error_values) || !empty($this->error_keys)) {
+			if (!empty($this->error_keys) || !empty($this->error_values)) {
 				return false;
 			}
 		}
@@ -163,23 +163,23 @@ class Dictionary extends Input implements IInformation, /*IErrorMessage,*/ ISche
 		//key input
 		if (isset($this->key_input)) {
 			//input labels
-			$input_label = isset($this->input) ? $this->input->getLabel($text_options, $info_options) : '...'; //TODO: change ... to Any input label
 			$key_input_label = $this->key_input->getLabel($text_options, $info_options);
-			if (UText::multiline($input_label) || UText::multiline($key_input_label)) {
-				$input_label = "\n" . UText::indentate($input_label) . "\n";
+			$input_label = isset($this->input) ? $this->input->getLabel($text_options, $info_options) : '...'; //TODO: change ... to Any input label
+			if (UText::multiline($key_input_label) || UText::multiline($input_label)) {
 				$key_input_label = "\n" . UText::indentate($key_input_label) . "\n" . UText::indentate('');
+				$input_label = "\n" . UText::indentate($input_label) . "\n";
 			}
 			
 			//label
 			/**
-			 * @placeholder input.label The input label.
 			 * @placeholder key_input.label The key input label.
+			 * @placeholder input.label The input label.
 			 * @example Dictionary<Integer:Text>
 			 */
 			return UText::localize("Dictionary<{{key_input.label}}:{{input.label}}>", self::class, $text_options, [
 				'parameters' => [
-					'input' => ['label' => $input_label],
-					'key_input' => ['label' => $key_input_label]
+					'key_input' => ['label' => $key_input_label],
+					'input' => ['label' => $input_label]
 				]
 			]);
 		}
@@ -211,10 +211,105 @@ class Dictionary extends Input implements IInformation, /*IErrorMessage,*/ ISche
 	/** {@inheritdoc} */
 	public function getDescription(TextOptions $text_options, InfoOptions $info_options): string
 	{
+		//input description
+		$input_description = isset($this->input) ? $this->input->getDescription($text_options, $info_options) : null;
+		if (isset($input_description)) {
+			$input_description = UText::formatMessage($input_description, true);
+		}
+		
+		//key input description
+		$key_input_description = isset($this->key_input)
+			? $this->key_input->getDescription($text_options, $info_options)
+			: null;
+		if (isset($key_input_description)) {
+			$key_input_description = UText::formatMessage($key_input_description, true);
+		}
+		
+		//inputs
+		if (isset($input_description) && isset($key_input_description)) {
+			//key input description
+			/**
+			 * @description Key bullet point with input description.
+			 * @placeholder key_input.description The key input description.
+			 * @example with each key as: a number.
+			 */
+			$key_input_description = UText::localize(
+				"with each key as: {{key_input.description}}",
+				self::class, $text_options, [
+					'parameters' => [
+						'key_input' => ['description' => $key_input_description]
+					]
+				]
+			);
+			if (UText::multiline($key_input_description)) {
+				$key_input_description .= "\n";
+			}
+			$key_input_description = UText::bulletify($key_input_description, $text_options);
+			
+			//input description
+			/**
+			 * @description Value bullet point with input description.
+			 * @placeholder input.description The input description.
+			 * @example with each value as: a text.
+			 */
+			$input_description = UText::localize(
+				"with each value as: {{input.description}}",
+				self::class, $text_options, [
+					'parameters' => [
+						'input' => ['description' => $input_description]
+					]
+				]
+			);
+			if (UText::multiline($input_description)) {
+				$input_description .= "\n";
+			}
+			$input_description = UText::bulletify($input_description, $text_options);
+			
+			//inputs descriptions
+			$inputs_descriptions = "{$key_input_description}\n{$input_description}";
+			
+			//scalar
+			if ($this->key_input->isScalar() && $this->input->isScalar()) {
+				/**
+				 * @placeholder key_input.description The key input description.
+				 * @placeholder input.description The input description.
+				 * @example A dictionary, \
+				 * which may be given as a comma separated list of colon separated key-value pairs:
+				 *  &#8226; with each key as: a number.
+				 *  &#8226; with each value as: a text.
+				 */
+				return UText::localize(
+					"A dictionary, " . 
+					"which may be given as a comma separated list of colon separated key-value pairs:\n" . 
+					"{{inputs_descriptions}}",
+					self::class, $text_options, [
+						'parameters' => [
+							'inputs_descriptions' => $inputs_descriptions
+						]
+					]
+				);
+			}
+			
+			//default
+			/**
+			 * @placeholder inputs_descriptions The inputs descriptions.
+			 * @example A dictionary:
+			 *  &#8226; with each key as: a number.
+			 *  &#8226; with each value as: a text.
+			 */
+			return UText::localize(
+				"A dictionary:\n{{inputs_descriptions}}",
+				self::class, $text_options, [
+					'parameters' => [
+						'inputs_descriptions' => $inputs_descriptions
+					]
+				]
+			);
+		}
+		
 		
 		//TODO
 		return '?';
-		
 	}
 	
 	/** {@inheritdoc} */
@@ -234,6 +329,7 @@ class Dictionary extends Input implements IInformation, /*IErrorMessage,*/ ISche
 	{
 		
 		//TODO
+		return null;
 		
 	}
 	
@@ -244,8 +340,8 @@ class Dictionary extends Input implements IInformation, /*IErrorMessage,*/ ISche
 	public function getSchemaData()
 	{
 		return [
-			'input' => isset($this->input) ? $this->input->getSchema() : null,
-			'key_input' => isset($this->key_input) ? $this->key_input->getSchema() : null
+			'key_input' => isset($this->key_input) ? $this->key_input->getSchema() : null,
+			'input' => isset($this->input) ? $this->input->getSchema() : null
 		];
 	}
 	
@@ -270,14 +366,14 @@ class Dictionary extends Input implements IInformation, /*IErrorMessage,*/ ISche
 	public function unsetError(): void
 	{
 		//errors
-		$this->error_values = $this->error_keys = [];
+		$this->error_keys = $this->error_values = [];
 		
 		//inputs
-		if (isset($this->input)) {
-			$this->input->unsetError();
-		}
 		if (isset($this->key_input)) {
 			$this->key_input->unsetError();
+		}
+		if (isset($this->input)) {
+			$this->input->unsetError();
 		}
 	}
 	
@@ -288,9 +384,9 @@ class Dictionary extends Input implements IInformation, /*IErrorMessage,*/ ISche
 	protected function buildProperty(string $name): ?Property
 	{
 		switch ($name) {
-			case 'input':
-				//no break
 			case 'key_input':
+				//no break
+			case 'input':
 				return $this->createProperty()->setMode('w-')->setAsComponent(Component::class)->bind(self::class);
 		}
 		return null;
