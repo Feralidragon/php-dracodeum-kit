e85f5b6d40dec2d71fb70f938ccb90e3433e3557
Cláudio "Feralidragon" Luís <tutano2004@gmail.com>
2019-07-12 17:43:39 +0100

    Removed labels and descriptions from InfoScope enumeration

--- src/Feralygon/Kit/Enumerations/InfoScope.php
+++ src/Feralygon/Kit/Enumerations/InfoScope.php
@@ -8,8 +8,6 @@
 namespace Feralygon\Kit\Enumerations;
 
 use Feralygon\Kit\Enumeration;
-use Feralygon\Kit\Utilities\Text as UText;
-use Feralygon\Kit\Options\Text as TextOptions;
 
 /**
  * This enumeration represents info scopes, which are used to define which kind of information to return 
@@ -28,47 +26,4 @@ class InfoScope extends Enumeration
 	
 	/** End-user info scope, for the user interacting with the application. */
 	public const ENDUSER = 2;
-	
-	
-	
-	//Implemented protected static methods (Feralygon\Kit\Enumeration\Traits\Information)
-	/** {@inheritdoc} */
-	protected static function returnLabel(string $name, TextOptions $text_options): ?string
-	{
-		switch ($name) {
-			case 'NONE':
-				/** @description "NONE" label. */
-				return UText::localize("None", self::class, $text_options);
-			case 'TECHNICAL':
-				/** @description "TECHNICAL" label. */
-				return UText::localize("Technical", self::class, $text_options);
-			case 'ENDUSER':
-				/** @description "ENDUSER" label. */
-				return UText::localize("End-user", self::class, $text_options);
-		}
-		return null;
-	}
-	
-	/** {@inheritdoc} */
-	protected static function returnDescription(string $name, TextOptions $text_options): ?string
-	{
-		switch ($name) {
-			case 'NONE':
-				/** @description "NONE" description. */
-				return UText::localize(
-					"No info scope specified.", self::class, $text_options
-				);
-			case 'TECHNICAL':
-				/** @description "TECHNICAL" description. */
-				return UText::localize(
-					"Technical info scope, for the developer creating the application.", self::class, $text_options
-				);
-			case 'ENDUSER':
-				/** @description "ENDUSER" description. */
-				return UText::localize(
-					"End-user info scope, for the user interacting with the application.", self::class, $text_options
-				);
-		}
-		return null;
-	}
 }
