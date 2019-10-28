efe696e5cfdf273968b50229c1407978c9580f81
Cláudio "Feralidragon" Luís <tutano2004@gmail.com>
2019-10-28 16:03:57 +0000

    Text input non-empty constraint ignore_whitespace property

--- src/Feralygon/Kit/Prototypes/Inputs/Text/Constraints/NonEmpty.php
+++ src/Feralygon/Kit/Prototypes/Inputs/Text/Constraints/NonEmpty.php
@@ -10,8 +10,10 @@ namespace Feralygon\Kit\Prototypes\Inputs\Text\Constraints;
 use Feralygon\Kit\Components\Input\Prototypes\Modifiers\Constraint;
 use Feralygon\Kit\Components\Input\Prototypes\Modifier\Interfaces\{
 	Subtype as ISubtype,
-	Information as IInformation
+	Information as IInformation,
+	SchemaData as ISchemaData
 };
+use Feralygon\Kit\Traits\LazyProperties\Property;
 use Feralygon\Kit\Options\Text as TextOptions;
 use Feralygon\Kit\Enumerations\InfoScope as EInfoScope;
 use Feralygon\Kit\Utilities\{
@@ -19,9 +21,20 @@ use Feralygon\Kit\Utilities\{
 	Type as UType
 };
 
-/** This constraint prototype prevents a given text input value from being empty. */
-class NonEmpty extends Constraint implements ISubtype, IInformation
+/**
+ * This constraint prototype prevents a given text input value from being empty.
+ * 
+ * @property-write bool $ignore_whitespace [writeonce] [transient] [coercive] [default = false]
+ * <p>Ignore whitespace characters from a given text input value.</p>
+ */
+class NonEmpty extends Constraint implements ISubtype, IInformation, ISchemaData
 {
+	//Protected properties
+	/** @var bool */
+	protected $ignore_whitespace = false;
+	
+	
+	
 	//Implemented public methods
 	/** {@inheritdoc} */
 	public function getName(): string
@@ -32,7 +45,7 @@ class NonEmpty extends Constraint implements ISubtype, IInformation
 	/** {@inheritdoc} */
 	public function checkValue($value): bool
 	{
-		return UType::evaluateString($value) && !UText::empty($value);
+		return UType::evaluateString($value) && !UText::empty($this->ignore_whitespace ? trim($value) : $value);
 	}
 	
 	
@@ -56,14 +69,57 @@ class NonEmpty extends Constraint implements ISubtype, IInformation
 	/** {@inheritdoc} */
 	public function getMessage(TextOptions $text_options): string
 	{
-		//technical
+		//message
+		$message = '';
 		if ($text_options->info_scope === EInfoScope::TECHNICAL) {
 			/** @tags technical */
-			return UText::localize("An empty string is not allowed.", self::class, $text_options);
+			$message = UText::localize("An empty string is not allowed.", self::class, $text_options);
+		} else {
+			/** @tags non-technical */
+			$message = UText::localize("An empty text is not allowed.", self::class, $text_options);
 		}
 		
-		//non-technical
-		/** @tags non-technical */
-		return UText::localize("An empty text is not allowed.", self::class, $text_options);
+		//ignore whitespace
+		if ($this->ignore_whitespace) {
+			$message .= "\n";
+			if ($text_options->info_scope === EInfoScope::ENDUSER) {
+				/** @tags end-user */
+				$message .= UText::localize(
+					"All space, tab and newline characters are ignored.", self::class, $text_options
+				);
+			} else {
+				/** @tags non-end-user */
+				$message .= UText::localize(
+					"All whitespace characters are ignored.", self::class, $text_options
+				);
+			}
+		}
+		
+		//return
+		return $message;
+	}
+	
+	
+	
+	//Implemented public methods (Feralygon\Kit\Components\Input\Prototypes\Modifier\Interfaces\SchemaData)
+	/** {@inheritdoc} */
+	public function getSchemaData()
+	{
+		return [
+			'ignore_whitespace' => $this->ignore_whitespace
+		];
+	}
+	
+	
+	
+	//Implemented protected methods (Feralygon\Kit\Prototype\Traits\PropertyBuilder)
+	/** {@inheritdoc} */
+	protected function buildProperty(string $name): ?Property
+	{
+		switch ($name) {
+			case 'ignore_whitespace':
+				return $this->createProperty()->setMode('w--')->setAsBoolean()->bind(self::class);
+		}
+		return null;
 	}
 }
