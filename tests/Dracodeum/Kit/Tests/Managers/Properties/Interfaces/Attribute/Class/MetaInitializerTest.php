c6024bff52876a4d7659c26bdb877fad754a3c19
Feralidragon <tutano2004@gmail.com>
2022-04-16 19:36:53 +0100

    PropertiesV2 manager attribute interfaces unit tests

---
+++ tests/Dracodeum/Kit/Tests/Managers/Properties/Interfaces/Attribute/Class/MetaInitializerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @author Cláudio "Feralidragon" Luís <claudioluis8@gmail.com>
+ * @license https://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Dracodeum\Kit\Tests\Managers\Properties\Interfaces\Attribute\Class;
+
+use PHPUnit\Framework\TestCase;
+use Dracodeum\Kit\Managers\PropertiesV2\Interfaces\Attribute\Class\MetaInitializer as IMetaInitializer;
+use Dracodeum\Kit\Managers\PropertiesV2 as Manager;
+use Dracodeum\Kit\Managers\PropertiesV2\Meta;
+use Dracodeum\Kit\Components\Type;
+use Attribute;
+
+/** @see \Dracodeum\Kit\Managers\PropertiesV2\Interfaces\Attribute\Class\MetaInitializer */
+class MetaInitializerTest extends TestCase
+{
+	//Public methods
+	/**
+	 * Test.
+	 * 
+	 * @testdox Test
+	 * 
+	 * @return void
+	 */
+	public function test(): void
+	{
+		//initialize
+		$meta = (new Manager(new MetaInitializerTest_Class()))->getMeta();
+		
+		//assert
+		$this->assertFalse($meta->has('m0'));
+		$this->assertTrue($meta->has('m1'));
+		$this->assertSame(123, $meta->get('m1')->default);
+	}
+}
+
+
+
+/** Test case attribute class. */
+#[Attribute(Attribute::TARGET_CLASS)]
+class MetaInitializerTest_Attribute implements IMetaInitializer
+{
+	public function initializeMeta(Meta $meta): void
+	{
+		$meta->set('m1', Type::build('int'), '123');
+	}
+}
+
+
+
+/** Test case dummy class. */
+#[MetaInitializerTest_Attribute]
+class MetaInitializerTest_Class {}
---
+++ tests/Dracodeum/Kit/Tests/Managers/Properties/Interfaces/Attribute/Property/PropertyInitializerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @author Cláudio "Feralidragon" Luís <claudioluis8@gmail.com>
+ * @license https://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Dracodeum\Kit\Tests\Managers\Properties\Interfaces\Attribute\Property;
+
+use PHPUnit\Framework\TestCase;
+use Dracodeum\Kit\Attributes\Class\propertyMeta;
+use Dracodeum\Kit\Managers\PropertiesV2\Interfaces\Attribute\Property\PropertyInitializer as IPropertyInitializer;
+use Dracodeum\Kit\Managers\PropertiesV2 as Manager;
+use Dracodeum\Kit\Managers\PropertiesV2\Property;
+use Attribute;
+
+/** @see \Dracodeum\Kit\Managers\PropertiesV2\Interfaces\Attribute\Property\PropertyInitializer */
+class PropertyInitializerTest extends TestCase
+{
+	//Public methods
+	/**
+	 * Test.
+	 * 
+	 * @testdox Test
+	 * 
+	 * @return void
+	 */
+	public function test(): void
+	{
+		//initialize
+		$manager = new Manager(new PropertyInitializerTest_Class());
+		
+		//assert
+		$this->assertSame(0, $manager->getProperty('p1')->getMetaValue('m1'));
+		$this->assertSame(123, $manager->getProperty('p2')->getMetaValue('m1'));
+	}
+}
+
+
+
+/** Test case attribute class. */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class PropertyInitializerTest_Attribute implements IPropertyInitializer
+{
+	public function initializeProperty(Property $property): void
+	{
+		$property->setMetaValue('m1', '123');
+	}
+}
+
+
+
+/** Test case dummy class. */
+#[propertyMeta('m1', 'int', 0)]
+class PropertyInitializerTest_Class
+{
+	public $p1;
+	
+	#[PropertyInitializerTest_Attribute]
+	public $p2;
+}
