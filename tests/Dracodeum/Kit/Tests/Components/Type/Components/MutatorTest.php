172a43c1fcc6546810c4c46a313347bf24ecc394
Feralidragon <tutano2004@gmail.com>
2022-05-07 18:32:53 +0100

    Mutator prototype ExplanationProducer unit test

--- tests/Dracodeum/Kit/Tests/Components/Type/Components/MutatorTest.php
+++ tests/Dracodeum/Kit/Tests/Components/Type/Components/MutatorTest.php
@@ -14,10 +14,7 @@ use Dracodeum\Kit\Primitives\{
 	Error,
 	Text
 };
-use Dracodeum\Kit\Components\Type\Prototypes\Mutator\Interfaces\{
-	Validator as IValidator,
-	ExplanationProducer as IExplanationProducer
-};
+use Dracodeum\Kit\Components\Type\Prototypes\Mutator\Interfaces\Validator as IValidator;
 use Dracodeum\Kit\Enumerations\InfoLevel as EInfoLevel;
 use Dracodeum\Kit\Utilities\Call\Exceptions\Halt as UCallHaltExceptions;
 use stdClass;
@@ -48,14 +45,6 @@ class MutatorTest extends TestCase
 		$this->assertSame(MutatorTest_Prototype1::ERROR_STRING, (string)$error1->getText());
 		
 		//value1 (error 2)
-		$value1 = $v1 = 123;
-		$error1 = $component1->process($value1);
-		$this->assertSame($v1, $value1);
-		$this->assertInstanceOf(Error::class, $error1);
-		$this->assertTrue($error1->hasText());
-		$this->assertSame(MutatorTest_Prototype1::EXPLANATION_STRING, (string)$error1->getText());
-		
-		//value1 (error 3)
 		$value1 = $v1 = new stdClass();
 		$error1 = $component1->process($value1);
 		$this->assertSame($v1, $value1);
@@ -126,29 +115,18 @@ class MutatorTest extends TestCase
 	 */
 	public function testExplanation(): void
 	{
-		//build
-		$component1 = Component::build(MutatorTest_Prototype1::class);
-		$component2 = Component::build(MutatorTest_Prototype2::class);
-		
-		//explanations
-		$explanation1 = $component1->getExplanation();
-		$explanation2 = $component2->getExplanation();
-		
-		//assert
-		$this->assertInstanceOf(Text::class, $explanation1);
-		$this->assertSame(MutatorTest_Prototype1::EXPLANATION_STRING, (string)$explanation1);
-		$this->assertNull($explanation2);
+		$this->assertNull(Component::build(MutatorTest_Prototype1::class)->getExplanation());
+		$this->assertNull(Component::build(MutatorTest_Prototype2::class)->getExplanation());
 	}
 }
 
 
 
 /** Test case dummy prototype class 1. */
-class MutatorTest_Prototype1 extends Prototype implements IExplanationProducer
+class MutatorTest_Prototype1 extends Prototype
 {
 	public const ERROR_STRING = "Must be equal to \"foo\".";
 	public const ERROR_STRING_TECHNICAL = "Cannot be an object.";
-	public const EXPLANATION_STRING = "Only a string equal to \"foo\" is allowed.";
 	
 	public function process(mixed &$value)
 	{
@@ -164,11 +142,6 @@ class MutatorTest_Prototype1 extends Prototype implements IExplanationProducer
 			return Error::build(text: Text::build(self::ERROR_STRING_TECHNICAL, EInfoLevel::TECHNICAL));
 		}
 		return Error::build(text: self::ERROR_STRING);
-	}
-	
-	public function produceExplanation()
-	{
-		return self::EXPLANATION_STRING;
 	}
 }
 
---
+++ tests/Dracodeum/Kit/Tests/Components/Type/Prototypes/Mutator/Interfaces/ExplanationProducerTest.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * @author Cláudio "Feralidragon" Luís <claudioluis8@gmail.com>
+ * @license https://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Dracodeum\Kit\Tests\Components\Type\Prototypes\Mutator\Interfaces;
+
+use PHPUnit\Framework\TestCase;
+use Dracodeum\Kit\Components\Type\Prototypes\Mutator\Interfaces\ExplanationProducer as IExplanationProducer;
+use Dracodeum\Kit\Components\Type\Components\Mutator as Component;
+use Dracodeum\Kit\Components\Type\Prototypes\Mutator as Prototype;
+use Dracodeum\Kit\Primitives\{
+	Error,
+	Text
+};
+
+/** @see \Dracodeum\Kit\Components\Type\Prototypes\Mutator\Interfaces\ExplanationProducer */
+class ExplanationProducerTest extends TestCase
+{
+	//Public methods
+	/**
+	 * Test.
+	 * 
+	 * @testdox Test
+	 * 
+	 * @return void
+	 */
+	public function test(): void
+	{
+		//build
+		$component1 = Component::build(ExplanationProducerTest_Prototype1::class);
+		$component2 = Component::build(ExplanationProducerTest_Prototype2::class);
+		
+		//explanations
+		$explanation1 = $component1->getExplanation();
+		$explanation2 = $component2->getExplanation();
+		
+		//assert
+		$this->assertInstanceOf(Text::class, $explanation1);
+		$this->assertInstanceOf(Text::class, $explanation2);
+		$this->assertSame(ExplanationProducerTest_Prototype1::STRING, (string)$explanation1);
+		$this->assertSame(ExplanationProducerTest_Prototype2::STRING, (string)$explanation2);
+		$this->assertNotSame((string)$explanation1, (string)$explanation2);
+	}
+	
+	/**
+	 * Test process.
+	 * 
+	 * @testdox Process
+	 * 
+	 * @return void
+	 */
+	public function testProcess(): void
+	{
+		//build
+		$component1 = Component::build(ExplanationProducerTest_Prototype1::class);
+		$component2 = Component::build(ExplanationProducerTest_Prototype2::class);
+		
+		//value (1)
+		$value = 10;
+		$error = $component1->process($value);
+		$this->assertInstanceOf(Error::class, $error);
+		$this->assertTrue($error->hasText());
+		$this->assertSame(ExplanationProducerTest_Prototype1::STRING, (string)$error->getText());
+		
+		//value (2)
+		$value = 100;
+		$error = $component2->process($value);
+		$this->assertInstanceOf(Error::class, $error);
+		$this->assertTrue($error->hasText());
+		$this->assertSame(ExplanationProducerTest_Prototype2::STRING, (string)$error->getText());
+	}
+}
+
+
+
+/** Test case dummy prototype class 1. */
+class ExplanationProducerTest_Prototype1 extends Prototype implements IExplanationProducer
+{
+	public const STRING = "Only a value greater than 50 is allowed.";
+	
+	public function process(mixed &$value)
+	{
+		return $value > 50;
+	}
+	
+	public function produceExplanation()
+	{
+		return self::STRING;
+	}
+}
+
+
+
+/** Test case dummy prototype class 2. */
+class ExplanationProducerTest_Prototype2 extends Prototype implements IExplanationProducer
+{
+	public const STRING = "Only a value lesser than 25 is allowed.";
+	
+	public function process(mixed &$value)
+	{
+		return $value < 25;
+	}
+	
+	public function produceExplanation()
+	{
+		return Text::build(self::STRING);
+	}
+}
